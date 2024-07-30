b1628a44d5b54034146d529e8438cc49dc8d7955
Andre Barros <andrebarros7201@gmail.com>
2024-07-30 13:08:23 +0100

    update queries and show product price

--- db/queries.js
+++ db/queries.js
@@ -2,7 +2,7 @@ const pool = require("./pool");
 
 exports.getProducts = async () => {
   const { rows } = await pool.query(
-    "SELECT products.name as Product, categories.type AS Type, manufacturers.name AS Manufacturer FROM products JOIN categories ON products.category_id = categories.id JOIN manufacturers ON products.manufacturer_id = manufacturers.id"
+    "SELECT products.name as Product, categories.type AS Type, manufacturers.name AS Manufacturer, products.price AS Price FROM products JOIN categories ON products.category_id = categories.id JOIN manufacturers ON products.manufacturer_id = manufacturers.id",
   );
   return rows;
 };
@@ -19,8 +19,8 @@ exports.getManufacturers = async () => {
 
 exports.getProductsByCategory = async (category) => {
   const { rows } = await pool.query(
-    "SELECT products.name as Product, categories.type AS Type, manufacturers.name AS Manufacturer FROM products JOIN categories ON products.category_id = categories.id JOIN manufacturers ON products.manufacturer_id = manufacturers.id WHERE category_id=(SELECT id FROM categories WHERE type = ($1))",
-    [category]
+    "SELECT products.name as Product, categories.type AS Type, manufacturers.name AS Manufacturer, products.price AS Price FROM products JOIN categories ON products.category_id = categories.id JOIN manufacturers ON products.manufacturer_id = manufacturers.id WHERE category_id=(SELECT id FROM categories WHERE type = ($1))",
+    [category],
   );
   return rows;
 };
@@ -28,6 +28,6 @@ exports.getProductsByCategory = async (category) => {
 exports.postProduct = async (name, type, manufacturer) => {
   await pool.query(
     "INSERT INTO products (name, category_id, manufacturer_id) VALUES ($1, $2, $3)",
-    [name, type, manufacturer]
+    [name, type, manufacturer],
   );
 };
